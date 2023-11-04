645d04d4492670b6c998e204e5f4de4e0755e9e4
Chris Andrews <chrisaa@umich.edu>
2023-11-04 09:12:27 -0400

    Added argument (csvdir) to write csv file "OPH_HVF.csv" to a specified directory.

--- R/xml_dir_extract.R
+++ R/xml_dir_extract.R
@@ -2,6 +2,7 @@
 #'
 #' @param xmldir (character) Name of directory containing xml files from FORUM
 #' @param recursive (logical) Should sub directories be included? Default is FALSE
+#' @param csvdir (character) Name of directory to write csv file(s) to. Default (\code{NULL}) is not to write files.
 #' @param comment (logical) Should text comment and patient comment be extracted? Default is FALSE.
 #' @param verbose (integer) Amount of output to the console.  0 is none. Higher values may produce more output. Default 0.
 #'
@@ -12,8 +13,21 @@
 xml_dir_extract <- function(
     xmldir,
     recursive = FALSE,
+    csvdir = NULL,
     comment = FALSE,
     verbose = 0) {
+
+  if (is.null(csvdir)) {
+    if (verbose > 0) cat("'csvdir' not specified. CSV data will not be exported/written.\n")
+  } else if (length(csvdir) != 1) {
+    cat("'csvdir' must be NULL or have length 1. CSV data will not be exported/written.\n")
+    csvdir <- NULL
+  } else if (isTRUE(dir.exists(csvdir))) {
+    if (verbose > 0) cat(sprintf("CSV data will be written to %s\n", csvdir))
+  } else {
+    cat(sprintf("Directory %s does not exist. CSV data will not be exported/written\n.", csvdir))
+    csvdir <- NULL
+  }
 
   # all xml filenames in directory xmldir
   xmlfilenames <- grep(
@@ -55,6 +69,14 @@ xml_dir_extract <- function(
     mat <- t(sapply(list_top, xml_extract, comment = comment)))
   if (isTRUE(verbose > 0)) {
     cat("Processing Duration", st2[1:3], "\n")
+  }
+
+  if (!is.null(csvdir)) {
+    utils::write.csv(
+      mat,
+      file = sprintf("%s/OPH_HVF.csv", csvdir),
+      row.names = FALSE,
+      na = "")
   }
 
   return(invisible(mat))
