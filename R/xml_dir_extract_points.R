4a12aec329a81744b19f61925712e7b71b759640
Chris Andrews <chrisaa@umich.edu>
2023-11-04 10:42:01 -0400

    Added output capability.

--- R/xml_dir_extract_points.R
+++ R/xml_dir_extract_points.R
@@ -2,6 +2,7 @@
 #'
 #' @param xmldir (character) Name of directory containing xml files from FORUM
 #' @param recursive (logical) Should sub directories be included? Default is FALSE
+#' @param csvdir (character) Name of directory to write csv file(s) to. Default (\code{NULL}) is not to write files.
 #' @param eyeformat See `xml_extract_points()`. Leave as \code{"OD"} (default).
 #' @param dropXY See `xml_extract_points()`.  Leave as \code{TRUE} (default).
 #' @param extra4fields See `xml_extract_points()`.  Leave as \code{FALSE} (default).
@@ -14,10 +15,23 @@
 xml_dir_extract_points <- function(
     xmldir,
     recursive = FALSE,
+    csvdir = NULL,
     eyeformat = "OD",
     dropXY = eyeformat %in% c("OD", "OS"),
     extra4fields = FALSE,
     verbose = 0) {
+
+  if (is.null(csvdir)) {
+    if (verbose > 0) cat("'csvdir' not specified. CSV data will not be exported/written.\n")
+  } else if (length(csvdir) != 1) {
+    cat("'csvdir' must be NULL or have length 1. CSV data will not be exported/written.\n")
+    csvdir <- NULL
+  } else if (isTRUE(dir.exists(csvdir))) {
+    if (verbose > 0) cat(sprintf("CSV data will be written to %s\n", csvdir))
+  } else {
+    cat(sprintf("Directory %s does not exist. CSV data will not be exported/written\n.", csvdir))
+    csvdir <- NULL
+  }
 
   # all xml filenames in directory xmldir
   xmlfilenames <- grep(
@@ -34,6 +48,9 @@ xml_dir_extract_points <- function(
 
   # list of xml documents
   startTime <- Sys.time()
+  if (isTRUE(verbose > 0)) {
+    cat(sprintf("Started reading at %s\n", startTime))
+  }
   st1 <-  system.time(
     list_parsed <- sapply(
       xmlfilenames,
@@ -41,7 +58,6 @@ xml_dir_extract_points <- function(
       simplify = FALSE,
       USE.NAMES = TRUE))
   if (isTRUE(verbose > 0)) {
-    cat(sprintf("Started reading at %s\n", startTime))
     cat("Reading Duration", st1[1:3], "\n")
   }
 
@@ -72,9 +88,31 @@ xml_dir_extract_points <- function(
   #   else return(attr(v, "TestPattern"))
   # })
   tp <- sapply(lst, FUN = function(v) v["TestPattern"])
+  testpatterns <- sort(unique(tp))
+  if (verbose > 0) {
+    cat(sprintf("%d unique test patterns reported\n", length(testpatterns)))
+    if (verbose > 1) {
+      print(table(tp))
+    }
+  }
   lstlst <- split(lst, tp)
   lstmat <- lapply(lstlst, FUN = function(lst) do.call(rbind, lst))
 
+  if (!is.null(csvdir)) {
+    lapply(
+      testpatterns,
+      FUN = function(testpattern) {
+        if (testpattern == "Unknown Pattern") return(NULL)
+        utils::write.csv(
+          lstmat[[testpattern]],
+          file = sprintf(
+            "%s/OPH_HVF_POINTS_%s.csv",
+            csvdir,
+            gsub(" ", "_", testpattern)),
+          row.names = FALSE,
+          na = "")
+      })
+  }
 
   return(invisible(lstmat))
 }
